eaa45eb9f87e53fe80f351d2b99f6e5900b22f21
hyo37009 <hyo37009@gmail.com>
2025-12-01 00:19:14 +0900

    fix: @RestControllerAdvice 주석처리
    - swagger 미작동으로 주석처리.

--- backend/src/main/java/com/opsw/backend/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/opsw/backend/handler/GlobalExceptionHandler.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.Instant;
 
-@RestControllerAdvice
+//@RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalArgumentException.class)
